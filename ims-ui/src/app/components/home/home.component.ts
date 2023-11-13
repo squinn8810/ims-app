aaa64121c80df3e173559567dc56820ebb06aad6
Christopher Hunter <25311970+Cazador314@users.noreply.github.com>
2023-11-12 20:06:01 -0500

    Formatting fixes

--- ims-ui/src/app/components/home/home.component.ts
+++ ims-ui/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login/login.service';
 
@@ -8,7 +8,7 @@ import { LoginService } from 'src/app/services/login/login.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   
   public accessToken: any;
   public accessTokenDetails: any;
@@ -24,6 +24,10 @@ export class HomeComponent {
       name: 'Test',
       email: 'test@email.com',
     };
+  }
+
+  public ngOnInit(): void {
+    // Call getUserInfo
   }
 
   logout(): void {
