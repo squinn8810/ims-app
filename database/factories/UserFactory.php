2b2bdca2badfd65bf5f74316ce5cfaaff177dfe3
steve-sephone <126113013+steve-sephone@users.noreply.github.com>
2023-11-15 19:41:19 -0500

    Analytical Tool (In Progress)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'id' => fake()->randomNumber(6),
+            'firstName' => fake()->firstName(),
+            'lastName' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'is_admin' => false, 
+            'is_superuser' => false,
         ];
     }
 
